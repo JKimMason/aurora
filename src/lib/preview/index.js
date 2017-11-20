f6fab0e9072d6a466a8048ab30ffa4e90f51f486
Kyle McCrohan <kmccrohan@zagmail.gonzaga.edu>
2017-11-19 22:21:45 -0800

    Got preview working with truncating content of notes that is too long

--- src/lib/preview/index.js
+++ src/lib/preview/index.js
@@ -2,6 +2,8 @@ import Preview from "./Preview.js";
 import React from "react";
 import { deSerializeContent, EDITOR_NAME } from "../editor";
 
+const MAX_PREVIEW_LENGTH = 40;
+
 function renderPreview(preview) {
   return <Preview preview={preview} />;
 }
@@ -12,9 +14,11 @@ function serializePreview(note) {
   }
   const editorState = deSerializeContent(note.content[EDITOR_NAME]);
   let text = editorState.getCurrentContent().getFirstBlock().text;
-  if (text === "") {
-    text = "New Note";
-  }
+  text = text === "" ? "New Note" : text;
+  text =
+    text.length > MAX_PREVIEW_LENGTH
+      ? text.substring(0, MAX_PREVIEW_LENGTH - 3) + "..."
+      : text;
   return text;
 }
 
